05439fbbaaff48654a86fcc34f039e884804b96b
AmirMohammadHakimiyan <118357842+AmirMohammadHakimiyan@users.noreply.github.com>
2023-05-24 11:23:55 -0700

    Add files via upload

---
+++ Assignment_32/2_median_filter.py
@@ -0,0 +1,19 @@
+import cv2 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+img = cv2.imread("inputs/xray.tif" , cv2.IMREAD_GRAYSCALE )
+
+x = img
+for i in range(4) :
+    x = cv2.medianBlur(x , 3 )
+x = cv2.medianBlur(x , 9 )
+
+horizontal = np.hstack((img , x))
+final_concat = np.concatenate((img , x), axis=1)
+
+cv2.imwrite("outputs/xray_MEDIAN_filter.jpg" ,final_concat )
+cv2.imshow('MEDIAN filter', final_concat)
+cv2.waitKey()
+
