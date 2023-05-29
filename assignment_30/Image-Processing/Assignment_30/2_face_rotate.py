bdc19c45c212ef1a8c23815d37f6ca51bbf95926
AmirMohammadHakimiyan <118357842+AmirMohammadHakimiyan@users.noreply.github.com>
2023-05-29 09:42:35 -0700

    Update 2_face_rotate.py

--- assignment_30/Image-Processing/Assignment_30/2_face_rotate.py
+++ assignment_30/Image-Processing/Assignment_30/2_face_rotate.py
@@ -3,7 +3,6 @@ import cv2
 from TFLiteFaceDetector import UltraLightFaceDetecion
 from TFLiteFaceAlignment import CoordinateAlignmentModel
 
-
 def roi_manipulations(ROI):
     global cropped_roi
     roi_landmarks = []
@@ -20,7 +19,6 @@ def roi_manipulations(ROI):
 
     return img 
 
-
 img = cv2.imread("inputs/girl.jpg")
 fd = UltraLightFaceDetecion("OpenVtuber\weights\RFB-320.tflite",conf_threshold=0.88)
 fa = CoordinateAlignmentModel("OpenVtuber\weights\coor_2d106.tflite")
@@ -29,22 +27,15 @@ face_cascade = cv2.CascadeClassifier('OpenVtuber\haarcascade_frontalface_default
 color = (0, 0, 255)
 boxes, scores = fd.inference(img)
 
-
 lips      = [52 , 64 , 63 , 71 , 67 , 68 , 61 , 58 , 59 , 53 , 56 , 55 ]
 left_eye  = [39 , 37 , 33 , 36 , 35 , 41 , 40 , 42 ]
 right_eye = [95 , 94 , 96 , 93 , 91 , 87 , 90 , 89 ]
-
-
-
 for pred in fa.get_landmarks(img , boxes):
-
     lip       = roi_manipulations(lips)
     left_eye  = roi_manipulations(left_eye)
     right_eye = roi_manipulations(right_eye)
 
-
 img = cv2.rotate(img ,rotateCode = cv2.ROTATE_180 ) 
-
 cv2.imshow("result", img)
 cv2.waitKey()
     
